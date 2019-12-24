b22673f06c86acf330749c95c11ac8ec1c6db1a7
Emeric Baha <emericbaha@gmail.com>
2019-12-24 15:35:18 +0100

    k

---
+++ client/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+      <BadGame/>
+  </div>
+</template>
+
+<script>
+    import BadGame from './components/BadGame.vue'
+
+    export default {
+      name: 'app',
+      components: {
+        BadGame
+      }
+    }
+</script>
+
+<style>
+</style>
