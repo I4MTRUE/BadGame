baa1f0824c86fd6a8b7fae1cf90e3f87ae8d6d1b
I4MTRUE <doge@DOGEPC.localdomain>
2019-12-30 00:29:56 +0100

    now with keyboard

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # BadGame
 
 Jeu multijoueur
+
+Fait avec Vue : https://vuejs.org/
+Socket.io : https://socket.io/
+et avec nodemon : https://www.npmjs.com/package/nodemon 
+(du coup avec node.js mais bon)
--- client/src/components/BadGame.vue
+++ client/src/components/BadGame.vue
@@ -12,11 +12,15 @@
         <button v-on:click="move('up')">Up</button>
         <button v-on:click="move('down')">Down</button>
     </p>
+    <p>TOUCHES : ZQSD </p>
   </div>
 </template>
+<script> 
 
+</script>
 <script>
   import io from "socket.io-client"
+  
   export default {
     name: 'BadGame',
     data() {
@@ -33,6 +37,12 @@
       this.socket = io("http://localhost:3000"); //à adapter selon l'addresse du serveur
     },
     mounted() {
+      window.addEventListener("keypress", e => {
+      if(String.fromCharCode(e.keyCode) == "d"){this.socket.emit("move", "right");}
+      if(String.fromCharCode(e.keyCode) == "q"){this.socket.emit("move", "left");}
+      if(String.fromCharCode(e.keyCode) == "z"){this.socket.emit("move", "up");}
+      if(String.fromCharCode(e.keyCode) == "s"){this.socket.emit("move", "down");}
+    });
       this.context = this.$refs.game.getContext("2d");
       this.socket.on("position", data => {
           this.position = data;
